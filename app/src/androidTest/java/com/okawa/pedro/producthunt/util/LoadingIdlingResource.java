a4b475aa4f3509cc21884297db8116968efc723e
PedroOkawa <pedro.henrique.okawa@gmail.com>
2016-02-26 03:12:49 +0000

    Fixed idling validation.

--- app/src/androidTest/java/com/okawa/pedro/producthunt/util/LoadingIdlingResource.java
+++ app/src/androidTest/java/com/okawa/pedro/producthunt/util/LoadingIdlingResource.java
@@ -1,10 +1,11 @@
 package com.okawa.pedro.producthunt.util;
 
 import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.support.test.espresso.IdlingResource;
 
-import com.okawa.pedro.producthunt.ui.loading.LoadingActivity;
+import com.okawa.pedro.producthunt.ui.main.MainActivity;
 
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class LoadingIdlingResource implements IdlingResource {
 
     @Override
     public boolean isIdleNow() {
-        boolean isIdle = isLoadingActivityRunning();
+        boolean isIdle = !isMainActivityRunning();
         if (isIdle && callback != null) {
             callback.onTransitionToIdle();
         }
@@ -39,18 +40,11 @@ public class LoadingIdlingResource implements IdlingResource {
         this.callback = callback;
     }
 
-    private boolean isLoadingActivityRunning() {
-        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.AppTask> appTasks = activityManager.getAppTasks();
-
-        for(ActivityManager.AppTask appTask : appTasks) {
-            if(appTask.getTaskInfo().baseActivity != null) {
-                if (LoadingActivity.class.getPackage().getName().equalsIgnoreCase(appTask.getTaskInfo().baseActivity.getPackageName())) {
-                    return true;
-                }
-            }
-        }
-        return false;
+    private boolean isMainActivityRunning() {
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> runningTaskInfo = manager.getRunningTasks(1);
+        ComponentName componentInfo = runningTaskInfo.get(0).topActivity;
+        return componentInfo.getClassName().equals(MainActivity.class.getName());
     }
 
 }
